93716b00c62f95e38e407da8c0de894ca61a987e
Diego Garcia <diegohans96@gmail.com>
2021-09-23 22:17:46 -0500

    Se quitaron los estilos y se comenzo a trabajar en el nuevo menu

--- Sistema/includes/scripts.php
+++ Sistema/includes/scripts.php
@@ -1,16 +1,20 @@
 <link rel="stylesheet" type="text/css" href="css/stylenav.css">
-<link rel="stylesheet" type="text/css" href="css/style2.css">
+
 <link rel="stylesheet" type="text/css" href="css/style3.css">
-<link rel="stylesheet" type="text/css" href="css/style4.css">
+<!--<link rel="stylesheet" type="text/css" href="css/style4.css">
+<link rel="stylesheet" type="text/css" href="css/style2.css">
 <link rel="stylesheet" type="text/css" href="css/style5.css">
 <link rel="stylesheet" type="text/css" href="css/style_foto.css">
 <link rel="stylesheet" type="text/css" href="css/style_foto1.css">
 <link rel="stylesheet" type="text/css" href="css/factura.css">
 <link rel="stylesheet" type="text/css" href="css/modal.css">
 <link rel="stylesheet" type="text/css" href="css/cotizacion_style.css">
-<link rel="stylesheet" type="text/css" href="css/Panel.css">
 <link rel="stylesheet" type="text/css" href="css/table.css">
+
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/icon.js"></script>
-<script type="text/javascript" src="js/funcions.js"></script>
+<script type="text/javascript" src="js/funcions.js"></script>-->
+
+<link rel="stylesheet" type="text/css" href="css/Panel.css">
+
 <?php include 'functions.php'?>
